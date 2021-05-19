3e14cbc907e4a99893c6ac1451e9cd18fb3c8683
Azzedine <az34bba@gmail.com>
2021-05-19 20:47:16 +0100

    PHP Syntax

---
+++ lessons-files/syntax.php
@@ -0,0 +1,19 @@
+<?php
+/** 
+		- A PHP script starts with [ <?php ] and ends with [?>]:
+		- [?>] If a file contains only PHP code, 
+			it is preferable to omit the PHP closing tag at the end of the file
+		- PHP statements end with a semicolon [;] 
+		- PHP is Case Sensitivity
+		- PHP is whitespace insensitive
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Learn PHP - Syntax</title>
+</head>
+<body>
+	<h1>PHP Syntax</h1>
+</body>
+</html>
