5e17063482ef681dc65b3535dd701b6a4dfd4594
Azzedine <az34bba@gmail.com>
2021-05-20 22:48:12 +0100

    Update PHP Constants

--- lessons-files/007-constants.php
+++ lessons-files/007-constants.php
@@ -13,6 +13,21 @@
 			Constants do not have a dollar sign ($) before them
 			Constants may be defined and accessed anywhere without regard to variable scoping rules
 			Constants may not be redefined or undefined once they have been set
+		- Predefined constants 
+			PHP provides a large number of predefined constants (Go to link below to see the full list)
+			https://www.php.net/manual/en/reserved.constants.php
+			Exps:
+				PHP_VERSION			=> Return The current PHP version
+				PHP_OS_FAMILY 		=> The operating system family PHP was built for
+				PHP_INT_MAX 		=> The largest integer supported in this build of PHP
+		- Magic constants:
+			There are nine magical constants that change depending on where they are used
+			https://www.php.net/manual/en/language.constants.magic.php
+			Exps:
+				__LINE__
+				__FILE__
+				__DIR__
+
 	*/
 ?>
 <!DOCTYPE html>
