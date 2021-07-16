7520a3335994ea8d33924c58ac4740b808721d43
azbba <az34bba@gmail.com>
2021-07-16 16:47:08 +0100

    PHP OOP - Inheritance (Override methods and properties, final keyword)

--- lessons-files/oop/004-inheritance.php
+++ lessons-files/oop/004-inheritance.php
@@ -7,28 +7,54 @@
 		- Inheritance allows you to write the code in the parent class and use it in both parent and child classes.
 		- An inherited class is defined by using the [ extends ] keyword.
 		- The child class may contain its own properties and methods 
+		- To override a property or method in child class 
+			you must use the same name of the property or the method in the child class
+		- To prevent the override use the [ final ] keyword
+			* for methods	=> You cannot override final methods or redeclare it (Use the method as what parent say). 
+			* for classs	=> You cannot inherit from the final class.
 		- Exp:
 */
 
-class User {
+final class User {
 	// Properties
 	public string $name;
 	public string $email;
 	public int $age;
+	protected string $password; 
 	// Methods
 	public function set_properties( $name, $email, $age ) {
 		$this->name = $name;
 		$this->email = $email;
 		$this->age = $age;
 	}
+	// Set password
+	final public function set_password( $password ) {
+		$this->password = sha1( filter_var( $password, FILTER_SANITIZE_STRING ) );
+	}
 }
 
 class Porfile extends User {
 	// All propertiess and methods of User are inherit, you can use it without create user class
+	public string $address;
+	public string $country;
+	public string $phone;
+	// Override set_properties() method
+	public function set_properties( $name, $email, $age, $address = '', $country = '', $phone = '' ) {
+		// Old method code
+		$this->name = $name;
+		$this->email = $email;
+		$this->age = $age;
+		// New method code
+		$this->address = $address;
+		$this->country = $country;
+		$this->phone = $phone;
+	}
+	// You cannot override the set_password()
 }
 
 $profile = new Porfile();
 $profile->set_properties( 'AzBba', 'az34bba@gmail.com', 29 );
+$profile->set_password( '1234567890' );
 
 echo '<pre>'; 
 	var_dump( $profile );
